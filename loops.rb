c7fc765c315267067e6b320850e839ba3969030a
keddo <adu.kedir.abdurahman@gmail.com>
2020-06-04 17:44:16 +0300

    the complement of while loop until

--- loops.rb
+++ loops.rb
@@ -1,7 +1,15 @@
 #the while loop
 
 counter = 1
-while counter < 11
-    puts counter
-    counter += 1 # counter++ doesn't work on ruby
+# while counter < 11
+#     puts counter
+#     counter += 1 # counter++ doesn't work on ruby
+# end
+
+#until loop
+
+i = 1;
+until i == 10
+    puts i;
+    i += 1; # it's possible to add semicolon in ruby
 end
