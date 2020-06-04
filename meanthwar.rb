287b35909d3f3c57938a2953dbbf7b680cc52743
keddo <adu.kedir.abdurahman@gmail.com>
2020-06-04 17:33:10 +0300

    Add comment descriptio to ruby functions`

--- meanthwar.rb
+++ meanthwar.rb
@@ -3,7 +3,7 @@ user_input = gets.chomp
 user_input.downcase!;
 
 if user_input.include? "s"
-    user_input.gsub!(/s/, 'th')
+    user_input.gsub!(/s/, 'th')  #gsub global substitution | include? function returns true or false
 else 
     puts "Nothing to do here."
 end
