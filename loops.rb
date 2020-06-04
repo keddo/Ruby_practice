ff70238008594f1424d173a96d8f35aa0be0e437
keddo <adu.kedir.abdurahman@gmail.com>
2020-06-04 17:45:48 +0300

    uncommented

--- loops.rb
+++ loops.rb
@@ -1,10 +1,10 @@
 #the while loop
 
 counter = 1
-# while counter < 11
-#     puts counter
-#     counter += 1 # counter++ doesn't work on ruby
-# end
+while counter < 11
+    puts counter
+    counter += 1 # counter++ doesn't work on ruby
+end
 
 #until loop
 
