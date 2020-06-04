02a2aaa989554013ce8b13702dd7c889d153e453
keddo <adu.kedir.abdurahman@gmail.com>
2020-06-04 16:26:12 +0300

    Add description commit to the unless stat

--- control_flow.rb
+++ control_flow.rb
@@ -1,7 +1,7 @@
 #unless -- used to check if something is foalse rather than true
 hungry = false
 
-unless hungry
+unless hungry   # this only can be executed if the variable hungry is false
   puts "I'm writing Ruby programs!"
 else
   puts "Time to eat!"
