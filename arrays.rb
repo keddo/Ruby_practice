377be00c5797f1fdf6e7a003658ada0c16a19ef6
keddo <adu.kedir.abdurahman@gmail.com>
2020-06-05 18:23:26 +0300

    How to define arrays in ruby

---
+++ arrays.rb
@@ -0,0 +1,9 @@
+#creating arrays
+array = [2, 3, 4]
+puts "array length: " + array.length.to_s
+array.each { |x|
+   print x.to_s + " "
+}
+puts ""
+#two dimentional arrays
+my_dimentional_array = [[2,3,4], [5, 5, 5], [6, 8 , 9]]
