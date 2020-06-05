edc3409111d7eddcbd80a808027341b9daef037a
keddo <adu.kedir.abdurahman@gmail.com>
2020-06-05 18:56:07 +0300

    Array can be created with multiple datatypes

--- arrays.rb
+++ arrays.rb
@@ -7,3 +7,8 @@ array.each { |x|
 puts ""
 #two dimentional arrays
 my_dimentional_array = [[2,3,4], [5, 5, 5], [6, 8 , 9]]
+
+#we can create an array of string and number
+my_array = [[2,3,4], [5, 2, 3], ["chedo", "wine and dine", "abe"]]
+
+array2 = ["dd", 2, 2.5, false] #array of multiple datatypes 
---
+++ hash.rb
@@ -0,0 +1,6 @@
+#hash the is similar with javascript objects
+hash = { "NY" => "Newyork", "PA" => "Pensilivania"}
+
+pets = Hash.new #creates new empty hash 
+
+pets["Key"] = "value"
